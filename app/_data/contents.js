e3fd92ca96dfd2201b68947ae3c86e9c232b1601
derikaquary <derikaquary@gmail.com>
2024-10-16 08:53:35 +0000

    committ again

--- app/_data/contents.js
+++ app/_data/contents.js
@@ -8,11 +8,11 @@ export const contents = [
     subImage3: "contMakanan",
     pageRef: "https://food-check.vercel.app/",
     text1:
-      "&laquo;🚀 Introducing My Latest Project&#58; The Calorie Check App &#128640;",
+      "&laquo;🚀 Introducing My Latest Project: The Calorie Check App &#128640;",
     text2:
-      "I&#39;m excited to share what I&#39;ve been working on&mdash;a cutting-edge app designed to help people stay informed about their nutritional intake with ease. With the Calorie Check app&#44; you can simply snap a picture of your food&#44; and the app will instantly analyze and display the calorie content. Whether you&#39;re on a fitness journey&#44; managing your diet&#44; or just curious about the calories in your meal&#44; this app makes it effortless.",
+      "I'm excited to share what I've been working on&mdash;a cutting-edge app designed to help people stay informed about their nutritional intake with ease. With the Calorie Check app, you can simply snap a picture of your food, and the app will instantly analyze and display the calorie content. Whether you're on a fitness journey, managing your diet, or just curious about the calories in your meal, this app makes it effortless.",
     text3:
-      "This project has been a rewarding challenge&#44; combining AI technology with user-friendly design to deliver a tool that empowers people to make healthier choices every day.",
+      "This project has been a rewarding challenge, combining AI technology with user-friendly design to deliver a tool that empowers people to make healthier choices every day.",
   },
   {
     id: "pdc",
@@ -23,11 +23,11 @@ export const contents = [
     subImage3: "plantCont",
     pageRef: "https://plant-check.vercel.app/",
     text1:
-      "I&#39;m excited to share my latest project&#44; PlantAnalyzer&#44; an innovative web application that empowers users to identify plants and diagnose potential issues with just a photo&#46; ",
+      "I'm excited to share my latest project, PlantAnalyzer, an innovative web application that empowers users to identify plants and diagnose potential issues with just a photo. ",
     text2:
-      "Instant Plant Analysis&#58; Simply take a picture or upload an image of a plant&#44; and the app will identify the plant species&#44; detect any possible issues&#44; and suggest specific solutions&#46;",
+      "Instant Plant Analysis: Simply take a picture or upload an image of a plant, and the app will identify the plant species, detect any possible issues, and suggest specific solutions.",
     text3:
-      "AI-Powered Insights&#58; Leveraging the power of Google&#39;s generative AI&#44; PlantAnalyzer provides detailed and accurate plant analysis&#44; including nutrient deficiencies and care recommendations&#46;",
+      "AI-Powered Insights: Leveraging the power of Google's generative AI, PlantAnalyzer provides detailed and accurate plant analysis, including nutrient deficiencies and care recommendations.",
   },
   {
     id: "oasis",
@@ -38,9 +38,9 @@ export const contents = [
     subImage3: "guest",
     pageRef: "https://wild-oasis-website-six.vercel.app/",
     text1:
-      "This a very ambitious project	&#44; where I expand my knowledge after learning React.js	&#44; and I continue to learn Next.js by building this project. ",
+      "This a very ambitious project	, where I expand my knowledge after learning React.js	, and I continue to learn Next.js by building this project. ",
     text2:
-      "This is a full-stack website where the backend is handled by using Supabase. Also&#44; I learned how to include an authentication process by using Auth.js",
+      "This is a full-stack website where the backend is handled by using Supabase. Also, I learned how to include an authentication process by using Auth.js",
   },
   {
     id: "ecommerce",
@@ -51,7 +51,7 @@ export const contents = [
     subImage3: "products",
     pageRef: "https://online-shopping-sooty-ten.vercel.app/",
     text1:
-      "This is where I started to learn react and apply useReducer to manage the state&#46; In this e-commerce-app&#44; the user can choose the item he wants&#44; can choose the amount using selector and put them in the cart&#46; In the cart page the total of the price will be calculated&#46;",
+      "This is where I started to learn react and apply useReducer to manage the state. In this e-commerce-app, the user can choose the item he wants, can choose the amount using selector and put them in the cart. In the cart page the total of the price will be calculated.",
   },
   {
     id: "chat",
@@ -62,9 +62,9 @@ export const contents = [
     subImage3: "signup",
     pageRef: "https://chatbuddy-derik-aquarys-projects.vercel.app/",
     text1:
-      "I&#39;m working on an exciting project with a friend to build an app similar to ChatGPT&#46; My role focuses on designing the UI/UX, using glassmorphism to create a modern and appealing interface, while my friend develops the AI for interactive conversations&#46; ",
+      "I'm working on an exciting project with a friend to build an app similar to ChatGPT. My role focuses on designing the UI/UX, using glassmorphism to create a modern and appealing interface, while my friend develops the AI for interactive conversations. ",
     text2:
-      "Our app aims to help users solve various problems or simply provide a friendly companion to chat with&#46; Although it's still under development&#44; I'm thrilled to be part of this journey and eager to see it come to fruition&#46;",
+      "Our app aims to help users solve various problems or simply provide a friendly companion to chat with. Although it's still under development, I'm thrilled to be part of this journey and eager to see it come to fruition.",
   },
   {
     id: "ummu",
@@ -75,6 +75,6 @@ export const contents = [
     subImage3: "order",
     pageRef: "https://warung-ummu-yassir.vercel.app/",
     text1:
-      "Excited to share my latest web development project&#33; I recently built a sleek&#44; user-friendly website for Ummu Yasir&#44; showcasing their authentic Indonesian cuisine&#46; From implementing a seamless design to optimizing for mobile responsiveness&#44; this site highlights my skills in creating engaging&#44; functional&#44; and visually appealing websites&#46; Check it out to see how I bring clients&#39; visions to life&#33;",
+      "Excited to share my latest web development project&#33; I recently built a sleek, user-friendly website for Ummu Yasir, showcasing their authentic Indonesian cuisine. From implementing a seamless design to optimizing for mobile responsiveness, this site highlights my skills in creating engaging, functional, and visually appealing websites. Check it out to see how I bring clients' visions to life&#33;",
   },
 ];
