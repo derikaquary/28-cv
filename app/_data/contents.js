89e6c6715d1f8bd08a66340774435595576cf8e9
derikaquary <derikaquary@gmail.com>
2024-10-16 08:30:17 +0000

    added text to pdc

--- app/_data/contents.js
+++ app/_data/contents.js
@@ -8,11 +8,11 @@ export const contents = [
     subImage3: "contMakanan",
     pageRef: "https://food-check.vercel.app/",
     text1:
-    "&laquo;🚀 Introducing My Latest Project&#58; The Calorie Check App &#128640;",
+      "&laquo;🚀 Introducing My Latest Project&#58; The Calorie Check App &#128640;",
     text2:
-      "I&#39;m excited to share what I&#39;ve been working on&mdash;a cutting-edge app designed to help people stay informed about their nutritional intake with ease. With the Calorie Check app, you can simply snap a picture of your food, and the app will instantly analyze and display the calorie content. Whether you&#39;re on a fitness journey, managing your diet, or just curious about the calories in your meal, this app makes it effortless.",
+      "I&#39;m excited to share what I&#39;ve been working on&mdash;a cutting-edge app designed to help people stay informed about their nutritional intake with ease. With the Calorie Check app&#44; you can simply snap a picture of your food&#44; and the app will instantly analyze and display the calorie content. Whether you&#39;re on a fitness journey&#44; managing your diet&#44; or just curious about the calories in your meal&#44; this app makes it effortless.",
     text3:
-      "This project has been a rewarding challenge, combining AI technology with user-friendly design to deliver a tool that empowers people to make healthier choices every day.",
+      "This project has been a rewarding challenge&#44; combining AI technology with user-friendly design to deliver a tool that empowers people to make healthier choices every day.",
   },
   {
     id: "pdc",
@@ -23,11 +23,11 @@ export const contents = [
     subImage3: "plantCont",
     pageRef: "https://plant-check.vercel.app/",
     text1:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "I&#39;m excited to share my latest project&#44; PlantAnalyzer&#44; an innovative web application that empowers users to identify plants and diagnose potential issues with just a photo&#46; ",
     text2:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "Instant Plant Analysis&#58; Simply take a picture or upload an image of a plant&#44; and the app will identify the plant species&#44; detect any possible issues&#44; and suggest specific solutions&#46;",
     text3:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "AI-Powered Insights&#58; Leveraging the power of Google&#39;s generative AI&#44; PlantAnalyzer provides detailed and accurate plant analysis&#44; including nutrient deficiencies and care recommendations&#46;",
   },
   {
     id: "oasis",
