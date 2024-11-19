4e1d7994ea0a569e301ae881e435cf66e51badbd
derikaquary <162952502+derikaquary@users.noreply.github.com>
2024-11-19 13:02:35 +0800

    uploaded the html file for verification

--- app/layout.js
+++ app/layout.js
@@ -33,7 +33,8 @@ export default function RootLayout({ children }) {
         {/* End Google Tag Manager */}
         {/* Google Site Verification for Search Console */}
         <meta name="google-site-verification" content="G4P6V5D2tbftaHJblDEJsP2ay-EBvwsjp9DOtLBV0hY" />
-      </head>{/* google-site-verification=G4P6V5D2tbftaHJblDEJsP2ay-EBvwsjp9DOtLBV0hY */}
+        {/* google-site-verification=G4P6V5D2tbftaHJblDEJsP2ay-EBvwsjp9DOtLBV0hY */}
+      </head>
       <body className={`${rubik.className} bg-[#212429] relative`}>
         {/* Google Tag Manager (noscript) */}
         <noscript>
