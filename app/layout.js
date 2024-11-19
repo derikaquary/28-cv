084dbbf801de57783e9c3753e28caa1f15e2058e
derikaquary <162952502+derikaquary@users.noreply.github.com>
2024-11-19 10:13:53 +0800

    added Google Tag Manager, sitemap and robots

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Rubik } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const rubik = Rubik({
   subsets: ["latin"],
@@ -9,13 +10,39 @@ const rubik = Rubik({
 
 export const metadata = {
   title: "Personal CV",
-  description: "explain personal CV",
+  description: "Explain personal CV",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/* Google Tag Manager */}
+        <Script id="gtm-head" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5KZF6P8C');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
       <body className={`${rubik.className} bg-[#212429] relative`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5KZF6P8C"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
       </body>
     </html>
