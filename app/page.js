243ec4a6935e7891aefd3af3911f546e764d9d16
derikaquary <162952502+derikaquary@users.noreply.github.com>
2024-11-21 05:27:18 +0800

    deleted isOpen from main page

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Skill from "./_components/Skill";
 import Projects from "./_components/Projects";
 
 export default function Home() {
-  const [isOpen, setIsOpen] = useState(false);
+  
 
   return (
     <div className="flex flex-col items-center gap-10 sm:gap-[100px] px-3 sm:pb-5 pb-9">
