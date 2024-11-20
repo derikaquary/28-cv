d48745f6b97109baf0a73e4962341dcf89c628e5
derikaquary <162952502+derikaquary@users.noreply.github.com>
2024-11-21 05:26:41 +0800

    deleted handlesidebar from main page

--- app/page.js
+++ app/page.js
@@ -1,6 +1,3 @@
-"use client";
-
-import { useState } from "react";
 import Header from "./_components/Header";
 import FirstPage from "./_components/FirstPage";
 import Skill from "./_components/Skill";
@@ -9,17 +6,12 @@ import Projects from "./_components/Projects";
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
 
-  function handleSidebar() {
-    setIsOpen((isOpen) => !isOpen);
-  }
-
   return (
     <div className="flex flex-col items-center gap-10 sm:gap-[100px] px-3 sm:pb-5 pb-9">
       <Header />
       <FirstPage />
       <Skill />
       <Projects />
-      
     </div>
   );
 }
