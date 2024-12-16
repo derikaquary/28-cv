b3af5a7b47e0d8ff87bd1ab0707a3a9aa27cf723
Derik Aquary <derikaquary@gmail.com>
2024-12-17 06:30:48 +0800

    fix the bug that no image show up in seconde image of the PDC Carousel

--- app/page.js
+++ app/page.js
@@ -3,12 +3,9 @@ import FirstPage from "./_components/FirstPage";
 import Skill from "./_components/Skill";
 import Projects from "./_components/Projects";
 
-
-
 export default function Home() {
-  
-
   return (
+    /* test */
     <div className="flex flex-col items-center gap-10 sm:gap-[100px] px-3 sm:pb-5 pb-9">
       <Header />
       <FirstPage />
