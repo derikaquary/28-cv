8b12ba8ed76bf8a08b3d0690ebde6d872da55412
Derik Aquary <derikaquary@gmail.com>
2024-12-17 10:27:47 +0800

    deleted the background for testing

--- app/_components/FirstPage.js
+++ app/_components/FirstPage.js
@@ -14,7 +14,7 @@ function FirstPage() {
   return (
     <div
       id="first_page"
-      className="flex flex-col gap-4 items-center w-full max-w-4xl sm:flex-row sm:justify-center sm:gap-8 sm:items-center pt-[60px] sm:mt-[40px] bg-plantArt"
+      className="flex flex-col gap-4 items-center w-full max-w-4xl sm:flex-row sm:justify-center sm:gap-8 sm:items-center pt-[60px] sm:mt-[40px]"
     >
       <div className="flex flex-col gap-3 ">
         <div>
